3d7c86232b8d2639a3e2e5a7d472c8a6040c8047
phoenixpina <phoenix.pina26@gmail.com>
2024-01-17 12:31:27 -0700

    Finished Prep 4.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+//using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        while (userNumber != 0) {
+            Console.Write("Enter a number. Enter 0 to quit. ");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0) {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach(int number in numbers) {
+            sum += number;
+        }
+
+        //float average = float.Parse(sum) / numbers.Count;
+        float sum2 = (float)sum;
+        float average = sum2 / numbers.Count;
+
+        int max = numbers[0];
+        foreach (int number in numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        Console.Write($"The sum is {sum}. ");
+        Console.Write($"The average is {average}. ");
+        Console.Write($"The highest number is {max}. ");
     }
 }
