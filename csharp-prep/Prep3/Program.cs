df97f9d48f3acfeaa9a1d20c93a7e8abe3e74f61
phoenixpina <phoenix.pina26@gmail.com>
2024-01-17 12:02:43 -0700

    Finished Prep 3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1,101);
+        int guess = -1;
+
+        while (guess != magicNumber) {
+            Console.Write($"What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber) {
+                Console.Write($"Guess lower. ");
+            }
+
+            else if (guess< magicNumber) {
+                Console.Write($"Guess higher. ");
+            }
+
+            else {
+                Console.Write($"You guessed correctly. ");
+            }
+        }
     }
 }
